3ff17f9f8a1b39f7b26c705ef2bfba2744a02e1d
ismet <iscometbg@gmail.com>
2026-01-05 20:45:21 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "priorityQueue.h"
 #include <iostream>
-#include <random>
+#include <ctime>
+#include <cstdlib>
+#include <string>
 
 void printMenu()
 {
@@ -10,30 +12,71 @@ void printMenu()
 
 void task1()
 {
-    
+    priority_queue<int> pq;
+    std::cout << "Please enter an integer and a path to a file in this format -> N path\n";
+
+    int N;
+    std::string path;
+    std::cin >> N >> path;
+
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    for (int i = 0; i < N; ++i)
+    {
+        int value = rand() % 100 + 1;
+        int priority = rand() % 35 + 1;
+        pq.enqueue(value, priority);
+    }
+
+    std::ofstream ofs(path);
+    if (!ofs.is_open())
+    {
+        std::cerr << "Could not open file for writing: " << path << '\n';
+        return;
+    }
+
+    pq.serialize(ofs);
 }
 
-void task2()
+void task2(priority_queue<int> &pq)
 {
+    priority_queue<int> copyPq;
+    std::cout << "Please enter path to file:\n";
+    std::string path;
+    std::cin >> path;
+    std::ifstream ifs(path);
 
+    if (!ifs.is_open())
+    {
+        std::cerr << "Could not open file for reading: " << path << '\n';
+        return;
+    }
+
+    copyPq.deserialize(ifs);
+
+    pq.swap(copyPq);
 }
 
-void task3()
+void task3(priority_queue<int> pq)
 {
-
+    std::cout<< "Elements in priority_queue: " << pq.size();
+    while(pq.size() != 0)
+    {
+        std::cout<< pq.head() << '\n';
+        pq.dequeue();
+    }
 }
 
 int main(int argc, char *argv[])
 {
     printMenu();
+    priority_queue<int> pqMain;
 
     int options = -1;
     std::cin >> options;
     while (options != 4)
     {
         std::cout << "\n\n";
-        printMenu();
-        std::cin >> options;
 
         switch (options)
         {
@@ -42,14 +85,16 @@ int main(int argc, char *argv[])
             break;
 
         case 2:
-            task2();
+            task2(pqMain);
             break;
 
         case 3:
-            task3();
+            task3(pqMain);
             break;
         }
+        printMenu();
+        std::cin >> options;
     }
 
-    std::cout << "Bye bye!\n"
+    std::cout << "Bye bye!\n";
 }
--- priorityQueue.h
+++ priorityQueue.h
@@ -1,16 +1,14 @@
 #pragma once
-#include <vector>
-#include <list>
-#include <map>
-#include <fstream>
 #include <iostream>
-#include <algorithm>
-#include <exception>
+#include <vector>
+#include <map>
+#include <list>
+#include <ostream>
+#include <fstream>
 
 template <typename T>
 class priority_queue
 {
-private:
     struct Node
     {
         int priority;
@@ -19,32 +17,208 @@ private:
 
     int heapSize;
     int elementCount;
-    std::vector<Node> binaryHeap;
-    std::map<int, int> priorityToNodeIndex;
+    std::vector<Node> binaryHeap;           // tuk shte pazq prioritetite
+    std::map<int, int> priorityToNodeIndex; // self explanatory
 
-    int parentIndex(int i) const { return (i == 0) ? 0 : (i - 1) / 2; }
-    int leftChildIndex(int i) const { return 2 * i + 1; }
-    int rightChildIndex(int i) const { return 2 * i + 2; }
+    int parent(int i)
+    {
+        if (i != 0)
+        {
+            return (i - 1) / 2;
+        }
+        else
+        {
+            std::cout << "Already at root, returning 0";
+            return 0;
+        }
+    }
 
-    void shiftUp(int i);
-    void shiftDown(int i);
+    int leftChild(int i) { return (2 * i + 1); }
+
+    int rightChild(int i) { return (2 * i + 2); }
+
+    void shiftUp(int i)
+    {
+        while (i > 0 && binaryHeap[parent(i)].priority > binaryHeap[i].priority)
+        {
+            std::swap(priorityToNodeIndex.at(binaryHeap[parent(i)].priority),
+                      priorityToNodeIndex.at(binaryHeap[i].priority));
+            std::swap(binaryHeap[parent(i)], binaryHeap[i]);
+            i = parent(i);
+        }
+    }
+
+    void shiftDown(int i)
+    {
+        int maxIndex = i;
+        int l = leftChild(i);
+        if (l < this->heapSize && this->binaryHeap[l].priority < this->binaryHeap[maxIndex].priority)
+            maxIndex = l;
+
+        int r = rightChild(i);
+        if (r < this->heapSize && this->binaryHeap[r].priority < this->binaryHeap[maxIndex].priority)
+            maxIndex = r;
+
+        if (maxIndex != i)
+        {
+            std::swap(priorityToNodeIndex.at(binaryHeap[maxIndex].priority),
+                      priorityToNodeIndex.at(binaryHeap[i].priority));
+            std::swap(binaryHeap[maxIndex], binaryHeap[i]);
+
+            shiftDown(maxIndex);
+        }
+    }
 
 public:
+    priority_queue() : elementCount(0), heapSize(0) {}
+
+    priority_queue(const priority_queue &src)
+    {
+        this->binaryHeap = src.binaryHeap;
+        this->priorityToNodeIndex = src.priorityToNodeIndex;
+        this->heapSize = src.heapSize;
+        this->elementCount = src.elementCount;
+    }
+
+    priority_queue<T> &operator=(const priority_queue<T> &rhs)
+    {
+        std::map<int, int> safeMapCopy(rhs.priorityToNodeIndex);
+        std::vector<Node> safeHeapCopy(rhs.binaryHeap);
+        int elements = rhs.elementCount;
+        int nodes = rhs.heapSize;
+
+        std::swap(this->priorityToNodeIndex, safeMapCopy);
+        std::swap(this->binaryHeap, safeHeapCopy);
+        std::swap(this->elementCount, elements);
+        std::swap(this->heapSize, nodes);
+
+        return *this;
+    }
+
+    ~priority_queue() {}
+
+    size_t size() const // O log 1
+    {
+        return this->elementCount;
+    }
+
+    T &head() // мисля че го показва само O(1)
+    {
+        return binaryHeap[0].values.front();
+    }
+
+    void enqueue(const T &element, int priority) // O log (n)
+    {
+        if (this->priorityToNodeIndex.find(priority) != priorityToNodeIndex.end())
+        {
+            this->binaryHeap[priorityToNodeIndex.at(priority)].values.push_back(element);
+            ++elementCount;
+        }
+        else
+        {
+            Node newNode;
+            newNode.priority = priority;
+            newNode.values.push_back(element);
+
+            binaryHeap.push_back(newNode);
+            priorityToNodeIndex[priority] = heapSize;
+            
+            shiftUp(heapSize);
+            ++elementCount;
+            ++heapSize;
+        }
+    }
+
+    void dequeue() // маха главата на опашката O (log n)
+    {
+        --elementCount;
+        binaryHeap[0].values.pop_front();
+        
+        if(binaryHeap[0].values.size() == 0)
+        {
+            
+            priorityToNodeIndex.erase(binaryHeap[0].priority);
+            binaryHeap[0] = binaryHeap.back();
+            binaryHeap.pop_back();
+
+            --heapSize;
+            shiftDown(0);
+        }
+    }
+
+    void clear() // изпразва опашката
+    {
+        binaryHeap.clear();
+        priorityToNodeIndex.clear();
+        heapSize = 0;
+        elementCount = 0;
+    }
+
+    void swap(priority_queue &other) noexcept
+    { // разменя съдържанието на два обекта за време O(1), nothrow
+        std::swap(this->binaryHeap, other.binaryHeap);
+        std::swap(this->priorityToNodeIndex, other.priorityToNodeIndex);
+        std::swap(this->heapSize, other.heapSize);
+        std::swap(this->elementCount, other.elementCount);
+    }
+
+    void serialize(std::ofstream &str) const // опашка-> поток
+    // времева О(N). хващат се всички възможни грешки, ако не успее операцията да се хвърли изключение
+    // не променя опашката по никакъв начинrhs
+    {   
+        if(str.bad()) {std::cerr << "Bad stream"; return;}
+        
+        str<< this->heapSize << ' ' << this->elementCount << '\n';
+        for(const Node& node: binaryHeap)
+        {
+            str << node.priority << ' ' << node.values.size() << ' ';
+
+            for(const T& value: node.values)
+            {
+                str<< value << ' ';
+            }
+            
+            str << '\n';
+        }
+    }
+
+    void deserialize(std::ifstream &out) // поток -> опашка
+    // ако вече има елементи, да се съхранят новите прочетени.
+    // времева О(N)
+    // хващат се всички грешки и ако не успее, се хвърля изглюючение.
+    // strong exception safety
+    {
+        if(!out.is_open()) {std::cerr << "IFSTREAM ISN'T OPEN, RETURNING. NO CHANGES!"; return;}
+        int copyHeapSize = 0;
+        int copyElementCount = 0;
+        std::vector<Node> copyHeap;
+        std::map<int,int>copyToNodeIndex;
+
+        out >> copyHeapSize >> copyElementCount;
+        int priority = -1;
+        int count = -1;
+        int currentIndex = 0;
+        while(out >> priority >> count)
+        {
+            Node newNode;
+            newNode.priority = priority;
+
+            for (int i = 0; i < count; ++i)
+            {
+                T temp;
+                out >> temp;
+                newNode.values.push_back(temp);
+            }
+
+            copyHeap.push_back(newNode);
+            copyToNodeIndex[priority] = currentIndex++;
+        }
     
-    priority_queue();
-    priority_queue(const priority_queue &src);
-    priority_queue &operator=(const priority_queue &rhs);
-    ~priority_queue() = default;
+        std::swap(this->binaryHeap, copyHeap); //mai
+        std::swap(this->priorityToNodeIndex,copyToNodeIndex);
+        std::swap(this->elementCount,copyElementCount);
+        std::swap(this->heapSize, copyHeapSize);
+    }
 
     
-    size_t size() const;
-    T &head();
-
-    void enqueue(const T &element, int priority);
-    void dequeue();
-    void clear();
-    void swap(priority_queue &other) noexcept;
-
-    void serialize(std::ofstream &str) const;
-    void deserialize(std::ifstream &out);
 };
